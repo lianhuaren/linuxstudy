900bd5e94075620c3e7d83044e69aa6320f84019
lianhuaren <lianhuaren@gmail.com>
2018-06-07 00:10:05 +0800

    first

---
+++ unp.h
@@ -0,0 +1,69 @@
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+#include	<sys/time.h>	/* timeval{} for select() */
+#include	<time.h>		/* timespec{} for pselect() */
+#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include	<arpa/inet.h>	/* inet(3) functions */
+#include	<errno.h>
+#include	<fcntl.h>		/* for nonblocking */
+#include	<netdb.h>
+#include	<signal.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#include	<sys/stat.h>	/* for S_xxx file mode constants */
+#include	<sys/uio.h>		/* for iovec{} and readv/writev */
+#include	<unistd.h>
+#include	<sys/wait.h>
+#include	<sys/un.h>		/* for Unix domain sockets */
+# include	<sys/select.h>	/* for convenience */
+# include	<pthread.h>
+
+#include	<stdarg.h>		/* ANSI C header file */
+#include	<syslog.h>		/* for syslog() */
+
+#define	MAXLINE		4096	/* max text line length */
+#define	SA	struct sockaddr
+#define	SERV_PORT		 9877
+#define	LISTENQ		1024
+
+
+
+
+void	 Close(int);
+
+/* prototypes for our socket wrapper functions: see {Sec errors} */
+int		 Accept(int, SA *, socklen_t *);
+void	 Bind(int, const SA *, socklen_t);
+void	 Connect(int, const SA *, socklen_t);
+void	 Getpeername(int, SA *, socklen_t *);
+void	 Getsockname(int, SA *, socklen_t *);
+void	 Getsockopt(int, int, int, void *, socklen_t *);
+int		 Isfdtype(int, int);
+void	 Listen(int, int);
+#ifdef	HAVE_POLL
+int		 Poll(struct pollfd *, unsigned long, int);
+#endif
+ssize_t	 Readline(int, void *, size_t);
+ssize_t	 Readn(int, void *, size_t);
+ssize_t	 Recv(int, void *, size_t, int);
+ssize_t	 Recvfrom(int, void *, size_t, int, SA *, socklen_t *);
+ssize_t	 Recvmsg(int, struct msghdr *, int);
+int		 Select(int, fd_set *, fd_set *, fd_set *, struct timeval *);
+void	 Send(int, const void *, size_t, int);
+void	 Sendto(int, const void *, size_t, int, const SA *, socklen_t);
+void	 Sendmsg(int, const struct msghdr *, int);
+void	 Setsockopt(int, int, int, const void *, socklen_t);
+void	 Shutdown(int, int);
+int		 Sockatmark(int);
+int		 Socket(int, int, int);
+void	 Socketpair(int, int, int, int *);
+void	 Writen(int, void *, size_t);
+
+void	 err_dump(const char *, ...);
+void	 err_msg(const char *, ...);
+void	 err_quit(const char *, ...);
+void	 err_ret(const char *, ...);
+void	 err_sys(const char *, ...);
+
+static void	err_doit(int, int, const char *, va_list);
